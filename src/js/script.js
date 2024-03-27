5f7f55103df9f4d603a2e9e571c5d415fd5fb9e8
PuzanovArtem <puzanov522@gmail.com>
2024-03-27 18:56:48 +0200

    initial header

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector('.burger');
+const content = document.querySelector('.header__content');
+const navLink = document.querySelectorAll('.nav-link');
+
+
+function openMenu(){
+    const activeBurger =  burger.classList.toggle('active');
+    const activeContent = content.classList.toggle('active');
+    if(activeBurger){
+        document.body.style.overflow = 'hidden';
+    }if(!activeBurger){
+        document.body.style.overflow = 'auto';
+    }
+}
+
+burger.addEventListener('click', openMenu)
+
+function closeMenu(){
+    burger.classList.remove('active');
+    content.classList.remove('active');
+    document.body.style.overflow = 'scroll';
+}
+
+navLink.forEach(n => n.addEventListener('click', closeMenu));
