d409b810574efe93463712a367bae82f3aa5d825
PuzanovArtem <puzanov522@gmail.com>
2024-04-03 20:27:46 +0300

    add working no adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// burger
+
 const burger = document.querySelector('.burger');
 const content = document.querySelector('.header__content');
 const navLink = document.querySelectorAll('.nav-link');
@@ -22,3 +24,21 @@ function closeMenu(){
 }
 
 navLink.forEach(n => n.addEventListener('click', closeMenu));
+
+// working
+
+$(function () {
+    let workingItems = $(".working__item")
+    workingItems.mouseover(function () {
+        $(".working__item-text").removeClass("working__item-text-active")
+        $(this).find(".working__item-text").addClass("working__item-text-active")
+        $(".working__item-number").removeClass("working__item-number-active")
+        $(this).find(".working__item-number").addClass("working__item-number-active")
+        $(".working__item-box").removeClass("working__item-box-active")
+        $(this).find(".working__item-box").addClass("working__item-box-active")
+    });
+    $(".backtests__slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1
+    })
+});
